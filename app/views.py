97bb8c79b2e5017fc1535c2c24659462e49abb2c
LUCAS TOMAZ NUNES <lucastnunes2@gmail.com>
2026-03-16 10:03:08 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from .models import *
+from .models import * 
+from django.contrib import messages 
 from django.views import View
+
 
 from django.shortcuts import render  # Added missing import
 
